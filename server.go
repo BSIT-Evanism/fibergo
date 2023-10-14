ec88b3746c2fb6ec5f0ce15a03d7dcb44d681934
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-14 20:12:23 +0800

    added more route

--- server.go
+++ server.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
@@ -9,5 +14,15 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Get("/more", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Evan!")
+	})
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
